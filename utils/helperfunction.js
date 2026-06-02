0c21f1fb01cc9177e44ebb0af3172872619147a8
Sudhanshu Dixit <sudhanshudix27@gmail.com>
2026-06-02 09:29:04 +0530

    UTC to IST utility

--- utils/helperfunction.js
+++ utils/helperfunction.js
@@ -1,1 +1,13 @@
-hi
+const moment = require('moment-timezone');
+
+function convertUTCtoIST(dateTime){
+    if(!dateTime || !moment(dateTime, "YYYY-MM-DD HH:mm:ss", true).isValid()){
+        return null;
+    }
+
+    return moment(dateTime)
+        .utc().tz('Asia/Kolkata')
+        .format("YYYY-MM-DD HH:mm:ss")
+}
+
+module.exports = convertUTCtoIST
